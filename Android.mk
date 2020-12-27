ae33f3578d13a9a2b010214cc882e80a12313902
derfelot <derfelot.git@gmail.com>
2020-12-27 04:25:00 +0100

    Remove Sony stock camera app
    Unforunately, due to incompatibilities of libimageprocessorjni.so with Android 11 libgui, the Sony stock camera app isn't working for us currently.
    
    Remove it so it doesn't clash with LOS Snap for e.g. launching via camera button from lockscreen.

--- Android.mk
+++ Android.mk
@@ -18,13 +18,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),poplar)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := SemcCameraUI-xxhdpi-release
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := proprietary/priv-app/SemcCameraUI-xxhdpi-release/SemcCameraUI-xxhdpi-release.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
 endif
